242f5ef422a5d282bba80d9469ba4db365b9896f
Hiroki Kato <hirokikato1@gmail.com>
2023-07-31 11:19:39 -0400

    completes main dq objectives

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,11 @@ import AlbumCard from "./AlbumCard";
 import { albums } from "../data/albums";
 
 function AlbumList() {
+  console.log(albums);
+  let Cards = albums.map((album) => {
+    <AlbumCard name={album.name} image={album.image} genre={album.genre} />;
+  });
+
   return (
     <section className="albums">
       <AlbumCard
